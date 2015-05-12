bbcad793d65608f5c6867cc4fafbe6f6136a755b
Rostislav <smerost2@gmail.com>
2015-05-12 08:23:26 +0300

    Added app stub component

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -2,22 +2,9 @@
 
 var React = require('react');
 
-// var Board = require('./components/Board.react');
-// var RowBuilder = require('./lib/RowBuilder');
-// var MinesweeperStore = require('./stores/MinesweeperStore');
-// var MinesweeperAPI = require('./apis/MinesweeperAPI');
-// require('../style/base.less');
-
-// var rows = MinesweeperAPI.getRows();
-// if (!rows || rows.length == 0) {
-//   rows = RowBuilder(10);
-// }
-// MinesweeperAPI.setRows(rows);
-
-// var state = MinesweeperStore.getState();
+var TisApp = require('./components/TisApp');
 
 React.render(
-  // <Board rows={state.rows} isWon={state.isWon} isLost={state.isLost} />,
-  <div>TEST</div>,
+  <TisApp />,
   document.getElementById('root')
 );
