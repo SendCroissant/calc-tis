830e05f52188529fe30ae0f9e9e663515e5db9fb
Rostislav <smerost2@gmail.com>
2015-05-14 08:20:13 +0300

    Added handling of CHANGE_CURRENT action in InputStore

--- src/js/stores/InputStore.js
+++ src/js/stores/InputStore.js
@@ -1,71 +1,12 @@
 'use strict';
 
-// var AppDispatcher = require('../dispatcher/AppDispatcher');
+var _ = require('lodash');
+var AppDispatcher = require('../dispatcher/AppDispatcher');
 var EventEmitter = require('events').EventEmitter;
 var inherits = require('util').inherits;
-// var TodoConstants = require('../constants/TodoConstants');
+var InputConstants = require('../constants/InputConstants');
 
 var CHANGE_EVENT = 'change';
-
-// var _todos = {};
-
-// /**
-//  * Create a TODO item.
-//  * @param  {string} text The content of the TODO
-//  */
-// function create(text) {
-//   // Hand waving here -- not showing how this interacts with XHR or persistent
-//   // server-side storage.
-//   // Using the current timestamp + random number in place of a real id.
-//   var id = (+new Date() + Math.floor(Math.random() * 999999)).toString(36);
-//   _todos[id] = {
-//     id: id,
-//     complete: false,
-//     text: text
-//   };
-// }
-
-// /**
-//  * Update a TODO item.
-//  * @param  {string} id
-//  * @param {object} updates An object literal containing only the data to be
-//  *     updated.
-//  */
-// function update(id, updates) {
-//   _todos[id] = assign({}, _todos[id], updates);
-// }
-
-// /**
-//  * Update all of the TODO items with the same object.
-//  *     the data to be updated.  Used to mark all TODOs as completed.
-//  * @param  {object} updates An object literal containing only the data to be
-//  *     updated.
-
-//  */
-// function updateAll(updates) {
-//   for (var id in _todos) {
-//     update(id, updates);
-//   }
-// }
-
-// /**
-//  * Delete a TODO item.
-//  * @param  {string} id
-//  */
-// function destroy(id) {
-//   delete _todos[id];
-// }
-
-// /**
-//  * Delete all the completed TODO items.
-//  */
-// function destroyCompleted() {
-//   for (var id in _todos) {
-//     if (_todos[id].complete) {
-//       destroy(id);
-//     }
-//   }
-// }
 
 function InputStore() {
   EventEmitter.call(this);
@@ -79,6 +20,10 @@ inherits(InputStore, EventEmitter);
  */
 InputStore.prototype.addChangeListener = function(callback) {
   this.on(CHANGE_EVENT, callback);
+};
+
+InputStore.prototype.emitChange = function() {
+  this.emit(CHANGE_EVENT);
 };
 
 /**
@@ -110,7 +55,7 @@ InputStore.prototype.getVariants = function() {
  */
 InputStore.prototype.readWindowData = function() {
   this._params = window.AppDataParams.data;
-  this._variants = Object.keys(this._params);
+  this._variants = _.map(Object.keys(this._params), Number);
   this._current = this._variants.length && this._variants[0] || null;
 };
 
@@ -121,87 +66,30 @@ InputStore.prototype.removeChangeListener = function(callback) {
   this.removeListener(CHANGE_EVENT, callback);
 };
 
+/**
+ * Set the current variant number.
+ * @param {number} newVariant
+ */
+InputStore.prototype.setCurrent = function(newVariant) {
+  this._current = newVariant;
+};
 
+var instance = new InputStore();
 
-//   /**
-//    * Tests whether all the remaining TODO items are marked as completed.
-//    * @return {boolean}
-//    */
-//   areAllComplete: function() {
-//     for (var id in _todos) {
-//       if (!_todos[id].complete) {
-//         return false;
-//       }
-//     }
-//     return true;
-//   },
+// Register callback to handle all updates
+AppDispatcher.register(function(action) {
 
-//   emitChange: function() {
-//     this.emit(CHANGE_EVENT);
-//   },
+  switch(action.actionType) {
+    case InputConstants.CHANGE_CURRENT:
+      if (instance.getCurrent() !== action.newVariant) {
+        instance.setCurrent(action.newVariant);
+        instance.emitChange();
+      }
+      break;
 
+    default:
+      // no op
+  }
+});
 
-//   /**
-//    * @param {function} callback
-//    */
-//   removeChangeListener: function(callback) {
-//     this.removeListener(CHANGE_EVENT, callback);
-//   }
-// });
-
-// // Register callback to handle all updates
-// AppDispatcher.register(function(action) {
-//   var text;
-
-//   switch(action.actionType) {
-//     case TodoConstants.TODO_CREATE:
-//       text = action.text.trim();
-//       if (text !== '') {
-//         create(text);
-//         InputStore.emitChange();
-//       }
-//       break;
-
-//     case TodoConstants.TODO_TOGGLE_COMPLETE_ALL:
-//       if (InputStore.areAllComplete()) {
-//         updateAll({complete: false});
-//       } else {
-//         updateAll({complete: true});
-//       }
-//       InputStore.emitChange();
-//       break;
-
-//     case TodoConstants.TODO_UNDO_COMPLETE:
-//       update(action.id, {complete: false});
-//       InputStore.emitChange();
-//       break;
-
-//     case TodoConstants.TODO_COMPLETE:
-//       update(action.id, {complete: true});
-//       InputStore.emitChange();
-//       break;
-
-//     case TodoConstants.TODO_UPDATE_TEXT:
-//       text = action.text.trim();
-//       if (text !== '') {
-//         update(action.id, {text: text});
-//         InputStore.emitChange();
-//       }
-//       break;
-
-//     case TodoConstants.TODO_DESTROY:
-//       destroy(action.id);
-//       InputStore.emitChange();
-//       break;
-
-//     case TodoConstants.TODO_DESTROY_COMPLETED:
-//       destroyCompleted();
-//       InputStore.emitChange();
-//       break;
-
-//     default:
-//       // no op
-//   }
-// });
-
-module.exports = new InputStore();
+module.exports = instance;
